283af8c55df1dadeca7838e6c8f64bb3d0918462
VaccaroniAlberto <alberto.vaccaroni.studenti@isii.it>
2022-10-05 18:21:58 +0200

    inserita label con scritta CIAONE

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblCIAO = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(31, 171);
+            this.button1.Location = new System.Drawing.Point(21, 111);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 58);
+            this.button1.Size = new System.Drawing.Size(65, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,22 +46,36 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(225, 117);
+            this.progressBar1.Location = new System.Drawing.Point(150, 76);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(369, 54);
+            this.progressBar1.Size = new System.Drawing.Size(246, 35);
             this.progressBar1.TabIndex = 1;
+            // 
+            // lblCIAO
+            // 
+            this.lblCIAO.AutoSize = true;
+            this.lblCIAO.Font = new System.Drawing.Font("MV Boli", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCIAO.Location = new System.Drawing.Point(120, 176);
+            this.lblCIAO.Name = "lblCIAO";
+            this.lblCIAO.Size = new System.Drawing.Size(276, 85);
+            this.lblCIAO.TabIndex = 2;
+            this.lblCIAO.Text = "CIAONE";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.lblCIAO);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +83,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblCIAO;
     }
 }
 
